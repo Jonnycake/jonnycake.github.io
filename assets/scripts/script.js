12992c068a1adb97c009006d743a1f63ae6c2422
Jonnycake <jonstockton1416@gmail.com>
2025-12-02 16:42:06 -0500

    New Custom Theme
    - Hopefully fix the issue with overflowing text on useful links page
    - Add category filters to archives
    - Include category names on individual blog post pages
    - Simplify how we output categories on the project pages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,4 +12,29 @@
         toggle_icon.addClass('fa-caret-down');
         header_nav.addClass('active');
     });
+
+    const archive_list = $('ul.archive-list');
+    archive_list.filter = function(category) {
+        if (category === '') {
+            $(this).find('li').show();
+            return;
+        }
+
+        $(this).find('li').each(function() {
+            let categories = $(this).attr('data-categories').split(',');
+
+            if (categories.includes(category)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+
+    const category_filter = $('select.category-filter');
+    if (category_filter) {
+        category_filter.on('change', function() {
+            archive_list.filter($(this).val());
+        });
+    }
 })(jQuery);
